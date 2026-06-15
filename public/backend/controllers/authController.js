4d1379977eb323d3a7ab2e4dbd6f45d2c67300d5
vivek <vivekkumarshukla0101>
2026-06-15 17:59:58 +0530

    aerjhj

---
+++ public/backend/controllers/authController.js
@@ -0,0 +1,31 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+    
